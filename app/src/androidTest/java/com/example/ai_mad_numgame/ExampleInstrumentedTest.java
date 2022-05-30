4b018dbe6deec2f89a7d4581cb1db4db8cea8acc
shramitgupta <76399598+shramitgupta@users.noreply.github.com>
2022-05-30 12:45:09 +0530

    work done

--- app/src/androidTest/java/com/example/ai_mad_numgame/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ai_mad_numgame/ExampleInstrumentedTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;//code
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ExampleInstrumentedTest {//changes made
     @Test
     public void useAppContext() {
         // Context of the app under test.
